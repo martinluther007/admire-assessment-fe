de9750117f24d29bad4b84c33660ba4f2c562a01
Apple <apple@Apples-MacBook-Pro.local>
2023-09-03 03:30:16 +0100

    chore:finished frontend project

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,3 +1,9 @@
-export const signupUrl = `http://127.0.0.1:3000/api/v1/user/signup`;
-export const signinUrl = `http://127.0.0.1:3000/api/v1/user/signin`;
-export const applicationUrl = `http://127.0.0.1:3000/api/v1/applications`;
+// ------------localhost----------
+
+// export const signupUrl = `http://127.0.0.1:3000/api/v1/user/signup`;
+// export const signinUrl = `http://127.0.0.1:3000/api/v1/user/signin`;
+// export const applicationUrl = `http://127.0.0.1:3000/api/v1/applications`;
+
+export const signupUrl = `https://admire-assessment-production.up.railway.app/api/v1/user/signup`;
+export const signinUrl = `https://admire-assessment-production.up.railway.app/api/v1/user/signin`;
+export const applicationUrl = `https://admire-assessment-production.up.railway.app/api/v1/applications`;
